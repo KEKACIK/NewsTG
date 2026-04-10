c57e2247733b6f35a11c2745dc74c224b931e600
KEKACIK <arthur_0p@mail.ru>
2026-04-10 17:15:05 +0300

    add source repository GetAll, Get

--- internal/source/repository.go
+++ internal/source/repository.go
@@ -26,19 +26,63 @@ func (r *repository) Create(ctx context.Context, source *Source) error {
 
 	return nil
 }
+func (r *repository) GetAll(ctx context.Context, source Source) ([]Source, error) {
+	q := `
+		SELECT
+			id, name
+		FROM source
+	`
+	r.logger.DebugSQL(q)
+
+	rows, err := r.client.Query(ctx, q)
+	if err != nil {
+		return nil, err
+	}
+
+	sources := make([]Source, 0)
+
+	for rows.Next() {
+		var source Source
+
+		err = rows.Scan(&source.ID, &source.Name)
+		if err != nil {
+			return nil, err
+		}
+
+		sources = append(sources, source)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return sources, nil
+}
 
 func (r *repository) Get(ctx context.Context, id int) (Source, error) {
-	panic("unimplemented")
+	q := `
+		SELECT
+			id, name
+		FROM source
+		WHERE id = $1
+	`
+	r.logger.DebugSQL(q)
+
+	var source Source
+	err := r.client.QueryRow(ctx, q, id).Scan(&source.ID, &source.Name)
+	if err != nil {
+		return Source{}, err
+	}
+
+	return source, nil
 }
 
-func (r *repository) GetAll(ctx context.Context, source Source) ([]Source, error) {
-	panic("unimplemented")
-}
-
+// TODO
 func (r *repository) Update(ctx context.Context, source *Source) error {
 	panic("unimplemented")
 }
 
+// TODO
 func (r *repository) Delete(ctx context.Context, id int) error {
 	panic("unimplemented")
 }
