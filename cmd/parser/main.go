7ed7b9464002a60640cdcf085c277ded2aebf066
KEKACIK <arthur_0p@mail.ru>
2026-04-23 12:56:33 +0300

    Structure Ria News parser

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"newtg/internal/config"
+	"newtg/internal/service/parser"
+	"newtg/pkg/logging"
+	"newtg/pkg/postgresql"
 )
 
 func main() {
 	fmt.Println("HELLO PARSER")
+
+	cfg := config.NewConfig()
+	logger := logging.NewLogger(cfg.Debug)
+
+	client, err := postgresql.NewClient(context.Background(), 5, cfg.GetPostgresDsn())
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	rc := parser.NewRiaClient(client, logger, "РИА Новости", cfg.MaxNewsPerHourRia)
+	rc.PoolNews(context.Background())
 }
---
+++ internal/service/parser/ria.go
@@ -0,0 +1,216 @@
+package parser
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"newtg/internal/news"
+	"newtg/internal/source"
+	"newtg/pkg/logging"
+	"newtg/pkg/postgresql"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type RiaClient struct {
+	client postgresql.Client
+	logger *logging.Logger
+
+	sourceName string
+	url        string
+	dynamicUrl string
+
+	dataFormat     string
+	timeFormat     string
+	maxNewsPerHour int
+}
+
+type Post struct {
+	Title       string
+	Link        string
+	Content     string
+	LikeSum     int
+	PublishedAt time.Time
+}
+
+func NewRiaClient(
+	client postgresql.Client,
+	logger *logging.Logger,
+
+	sourceName string,
+	maxNewsPerHour int,
+) *RiaClient {
+	return &RiaClient{
+		client: client,
+		logger: logger,
+
+		sourceName:     sourceName,
+		url:            "https://ria.ru/services/tagsearch/?date_start=%s&date=%s&tags[]=world&view=tags",
+		dynamicUrl:     "https://ria.ru/services/dynamics/%s/%s.html",
+		dataFormat:     "20060102",
+		timeFormat:     "15:04 02.01.2006",
+		maxNewsPerHour: maxNewsPerHour,
+	}
+}
+
+func (rc *RiaClient) PoolNews(ctx context.Context) {
+	dataFrom := time.Now().AddDate(0, 0, -1)
+	dataTo := time.Now()
+
+	posts, err := rc.GetAllPosts(dataFrom, dataTo)
+	if err != nil {
+		log.Fatal("Ошибка чтения HTML:", err)
+	}
+
+	sourceRepo := source.NewRepository(rc.client, rc.logger)
+	rSource, err := sourceRepo.GetByName(context.Background(), rc.sourceName)
+	if err != nil {
+		rc.logger.Fatal(err.Error())
+	}
+
+	newsRepo := news.NewRepository(rc.client, rc.logger)
+
+	for _, post := range posts {
+		err = newsRepo.Create(context.Background(), &news.CreateNewsDTO{
+			Title:     post.Title,
+			Link:      post.Link,
+			Content:   post.Content,
+			SourceID:  rSource.ID,
+			Likes:     post.LikeSum,
+			Published: post.PublishedAt,
+		})
+		if err != nil {
+			rc.logger.Error(err.Error())
+		}
+	}
+}
+
+func (rc *RiaClient) GetAllPosts(dataFrom, dataTo time.Time) ([]*Post, error) {
+	hClient := &http.Client{Timeout: 10 * time.Second}
+
+	url := fmt.Sprintf(rc.url, dataFrom.Format(rc.dataFormat), dataTo.Format(rc.dataFormat))
+	res, err := hClient.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Ошибка: статус код %d", res.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	posts := make([]*Post, 0)
+
+	doc.Find(".list-item").Each(func(i int, s *goquery.Selection) {
+		link, exists := s.Find("a.list-item__title.color-font-hover-only").Attr("href")
+		if !exists {
+			rc.logger.Warn(err.Error())
+			return
+		}
+
+		post, err := rc.GetPost(hClient, link)
+		if err != nil {
+			rc.logger.Warn(err.Error())
+			return
+		}
+
+		posts = append(posts, post)
+	})
+
+	return posts, nil
+}
+
+func (rc *RiaClient) GetPost(hClient *http.Client, postUrl string) (*Post, error) {
+	res, err := hClient.Get(postUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Ошибка: статус код %d", res.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	post := &Post{}
+
+	post.Title = doc.Find("div.article__title").First().Text()
+
+	post.Link = postUrl
+
+	contentList := make([]string, 0)
+	doc.Find(".article__summary-list li").Each(func(i int, s *goquery.Selection) {
+		contentList = append(contentList, s.Text())
+	})
+	post.Content = strings.Join(contentList, "\n\n")
+
+	{
+		dynamicUrl, err := rc.getDynamicLink(postUrl)
+		if err != nil {
+			return nil, err
+		}
+
+		dynamicRes, err := hClient.Get(dynamicUrl)
+		if err != nil {
+			return nil, err
+		}
+		defer dynamicRes.Body.Close()
+
+		if dynamicRes.StatusCode != 200 {
+			return nil, fmt.Errorf("Ошибка: статус код %d", dynamicRes.StatusCode)
+		}
+
+		dynamicDoc, err := goquery.NewDocumentFromReader(dynamicRes.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		for i := 1; i <= 6; i++ {
+			countStr := dynamicDoc.Find(fmt.Sprintf(".emoji-item.m-type-s%d .m-value", i)).First().Text()
+			count, err := strconv.Atoi(countStr)
+			if err != nil {
+				return nil, err
+			}
+
+			if i <= 3 {
+				post.LikeSum += count
+			} else {
+				post.LikeSum -= count
+			}
+		}
+	}
+
+	post.PublishedAt, err = time.Parse(rc.timeFormat, doc.Find("div.article__info-date a").First().Text())
+	if err != nil {
+		return nil, err
+	}
+
+	return post, nil
+}
+
+func (rc *RiaClient) getDynamicLink(link string) (string, error) {
+	re := regexp.MustCompile(`ria\.ru/(\d{8})/.+-(\d+)\.html`)
+	matches := re.FindStringSubmatch(link)
+
+	if len(matches) < 3 {
+		fmt.Println("Не удалось распарсить ссылку")
+		return "", fmt.Errorf("Dynamic link error")
+	}
+
+	return fmt.Sprintf(rc.dynamicUrl, matches[1], matches[2]), nil
+}
