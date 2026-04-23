8454aac85eb0d1b5104ea4e5b434e39b9b855cde
KEKACIK <arthur_0p@mail.ru>
2026-04-23 12:44:09 +0300

    Update init.sql and sql logger

--- pkg/logging/sql.go
+++ pkg/logging/sql.go
@@ -2,10 +2,10 @@ package logging
 
 import "strings"
 
-func (l *Logger) DebugSQL(q string) {
+func (l *Logger) DebugSQL(q string, args ...any) {
 	q = strings.ReplaceAll(q, "\t", " ")
 	q = strings.ReplaceAll(q, "\n", "")
 	q = strings.TrimSpace(q)
 
-	l.Debug(q)
+	l.Debug(q, args...)
 }
--- scripts/init.sql
+++ scripts/init.sql
@@ -1,21 +1,23 @@
 CREATE TABLE IF NOT EXISTS source (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(100) NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO source(name) VALUES ('РИА Новости');
+
 
 CREATE TABLE IF NOT EXISTS news (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    link TEXT UNIQUE NOT NULL,
-    content TEXT,
-    source_id SERIAL NOT NULL,
-    CONSTRAINT source_fk FOREIGN KEY (source_id) REFERENCES source(id),
-    status VARCHAR(64) DEFAULT 'wait',
+    id              SERIAL PRIMARY KEY,
+    title           TEXT NOT NULL,
+    link            TEXT UNIQUE NOT NULL,
+    content         TEXT,
+    source_id       SERIAL NOT NULL,
+    status          VARCHAR(64) DEFAULT 'wait',
+    likes           INTEGER DEFAULT 0,
 
-    published TIMESTAMP,
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    published_at   TIMESTAMP,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
+    CONSTRAINT source_fk FOREIGN KEY (source_id) REFERENCES source(id)
 );
-
-CREATE INDEX IF NOT EXISTS idx_news_not_posted ON news(posted) WHERE posted = FALSE;
